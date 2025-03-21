57be508689e0ca8002bd249049d4dff66d278082
josuetorr <josue.dtorres@proton.me>
2025-03-21 19:50:00 -0400

    added boilerplate for basic lexer tests

---
+++ internal/lexer/lexer_test.go
@@ -0,0 +1,9 @@
+package lexer_test
+
+import "testing"
+
+func TestLexer_ShouldTokenizeBody_Successful(t *testing.T) {}
+
+func TestLexer_ShouldTokenizeHead_Successful(t *testing.T) {}
+
+func TestLexer_ShouldIgnoreStyleAndScriptTags_Successful(t *testing.T) {}
