ce5fac1cc941aa594a00ca39860834c8c258231d
josuetorr <josue.dtorres@proton.me>
2025-04-01 19:07:51 -0400

    removed code to optimize

--- cmd/nomad/main.go
+++ cmd/nomad/main.go
@@ -1,45 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"sync"
-
-	"github.com/josuetorr/nomad/internal/common"
 	"github.com/josuetorr/nomad/internal/db"
-	"github.com/josuetorr/nomad/internal/spidey"
-	"github.com/josuetorr/nomad/internal/zeno"
 )
 
 const startURL = "https://wikipedia.org/wiki/meme"
 
 func main() {
 	kv := db.NewKV("/tmp/badger/nomad")
-	pc := make(chan spidey.DocData, 5000)
-	spidey := spidey.NewSpidey(kv)
-	zeno := zeno.NewZeno(kv)
-	wg := &sync.WaitGroup{}
-
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		spidey.Crawl(startURL, pc)
-	}()
-
-	for range 5 {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			zeno.IndexTF(pc)
-		}()
-	}
-	wg.Wait()
-	zeno.IndexDF()
-
-	// testing stored data
-	bytes, err := kv.Get(common.DocCountKey())
-	if err != nil {
-		fmt.Printf("Failed to get doc_count. err: %s\n", err)
-	}
-	docN, _ := common.BytesToUint64(bytes)
-	fmt.Printf("doc count %d\n", docN)
 }
--- internal/zeno/zeno.go
+++ internal/zeno/zeno.go
@@ -1,15 +1,8 @@
 package zeno
 
 import (
-	"fmt"
-	"strings"
-	"sync"
-
-	"github.com/josuetorr/nomad/internal/common"
 	"github.com/josuetorr/nomad/internal/db"
-	"github.com/josuetorr/nomad/internal/lexer"
 	"github.com/josuetorr/nomad/internal/spidey"
-	"golang.org/x/net/html"
 )
 
 type (
@@ -19,88 +12,22 @@ type (
 	termFreq = map[term]docFreq
 )
 
-type Zeno struct {
-	kv db.KVStorer
-
-	mu   *sync.Mutex
-	tft  termFreq
-	docN uint64
-}
+type Zeno struct{}
 
 const mapSize = 500
 
 func NewZeno(kv db.KVStorer) Zeno {
-	return Zeno{
-		kv: kv,
-
-		mu:   &sync.Mutex{},
-		tft:  make(termFreq, mapSize),
-		docN: 0,
-	}
+	return Zeno{}
 }
 
 // IndexTF indexes the page received in cc channel by storing TermFrequence per Document.
 // The calculation for tf-idf is calculate when a search query is received
 func (z *Zeno) IndexTF(pc <-chan spidey.DocData) {
 	for doc := range pc {
-		if !doc.Indexable {
-			fmt.Printf("Skipping %s...\n", doc.Url)
-			continue
-		}
-
-		root, err := html.Parse(strings.NewReader(doc.Content))
-		if err != nil {
-			fmt.Printf("Failed to parse: %s. Error: %s", doc.Url, err)
-			continue
-		}
-		text := common.ExtractDocText(root)
-		l := lexer.NewLexer(text)
-
-		z.mu.Lock()
-		z.docN++
-		for _, t := range l.Tokens() {
-			t := string(t)
-			if _, ok := z.tft[t]; !ok {
-				z.tft[t] = make(docFreq, mapSize)
-			} else {
-				z.tft[t][doc.Url]++
-			}
-		}
-		z.mu.Unlock()
-
-		fmt.Printf("Indexing %s...\n", doc.Url)
-		z.mu.Lock()
-		err = z.kv.BatchWrite(func(w db.KVWriter) {
-			for term, docF := range z.tft {
-				key := []byte(common.TermKey(term))
-				var val []byte
-				i := 0
-				for docID, f := range docF {
-					if i == (len(docF) - 1) {
-						val = fmt.Appendf(val, "%s:%d", docID, f)
-					} else {
-						val = fmt.Appendf(val, "%s:%d,", docID, f)
-					}
-					i++
-				}
-				w.Set(key, val)
-			}
-		})
-		if err != nil {
-			fmt.Printf("Failed to batch write: %s. Error: %s", doc.Url, err)
-		}
-		z.mu.Unlock()
 	}
 }
 
 // IndexDF indexes documents by DocumentFrequency. This func should be called once IndexTF is done doing its job
 // since it expects o.tf to not change
 func (z *Zeno) IndexDF() {
-	z.kv.Put(common.DocCountKey(), common.Uint64ToBytes(z.docN))
-	z.kv.BatchWrite(func(w db.KVWriter) {
-		for t, docF := range z.tft {
-			k := common.DFKey(t)
-			w.Set([]byte(k), fmt.Appendf(nil, "%d", len(docF)))
-		}
-	})
 }
