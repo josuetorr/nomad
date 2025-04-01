6655e177f5a36dbdc16ad32eba7fcce862c508f7
josuetorr <josue.dtorres@proton.me>
2025-04-01 00:10:41 -0400

    moved common/interfaces to db/common

--- internal/db/common.go
+++ internal/db/common.go
@@ -1,10 +1,15 @@
-package common
+package db
 
 type KVStorer interface {
 	Put(key string, value []byte) error
 	Get(key string) ([]byte, error)
+	BatchWrite(fn func(w KVWriter)) error
 	IteratePrefix(key string, fn func(val []byte) error) error
 	Exists(key string) bool
 	Delete(key string) error
 	Close() error
+}
+
+type KVWriter interface {
+	Set(key []byte, val []byte) error
 }
