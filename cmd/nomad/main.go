64b01b01f0f51ca3ad90667fb2ca117b011e04a7
josuetorr <josue.dtorres@proton.me>
2025-04-02 01:43:50 -0400

    using pipeline

--- cmd/nomad/main.go
+++ cmd/nomad/main.go
@@ -1,11 +1,31 @@
 package main
 
 import (
-	"github.com/josuetorr/nomad/internal/db"
+	"fmt"
+	"sync"
+
+	"github.com/josuetorr/nomad/internal/pipeline"
 )
 
 const startURL = "https://wikipedia.org/wiki/meme"
 
 func main() {
-	kv := db.NewKV("/tmp/badger/nomad")
+	pl := pipeline.Init()
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		pl.Crawl(startURL)
+	}()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for dj := range pl.DocJobs {
+			for _, d := range dj {
+				fmt.Printf("%s\n", d.Url)
+			}
+		}
+	}()
+	wg.Wait()
+	println("done")
 }
