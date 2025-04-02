754fbb6fb1d58262973f4872ab18040070a76035
josuetorr <josue.dtorres@proton.me>
2025-04-02 01:54:30 -0400

    updated iterateprefix func

--- internal/db/common.go
+++ internal/db/common.go
@@ -4,7 +4,7 @@ type KVStorer interface {
 	Put(key string, value []byte) error
 	Get(key string) ([]byte, error)
 	BatchWrite(fn func(w KVWriter)) error
-	IteratePrefix(key string, fn func(val []byte) error) error
+	IteratePrefix(key string, fn func(key []byte, val []byte) error) error
 	Exists(key string) bool
 	Delete(key string) error
 	Close() error
